eca35fa7f87426af8c4aa892a14fc69ce46bc037
Sachin <raisethebat2020@gmail.com>
2024-09-02 00:10:43 +0530

    rendered macbook and opening while scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import {Canvas} from "@react-three/fiber"
+import "./style.css";
+import {Environment, ScrollControls} from "@react-three/drei"
+import MacContainer from "./MacContainer";
 
 const App = () => {
   return (
-    <div>
-      hi
-    </div>
-  )
-}
+    <Canvas camera={{ fov:12, position:[0, -10, 220]}}>
+      <Environment 
+        files={[
+        "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"
+        ]} 
+      />
+      <ScrollControls>
+        <MacContainer/>
+      </ScrollControls>
+      
+    </Canvas>
+  );
+};
 
 export default App
